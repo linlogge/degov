3c77f15d62f7a4fbdfca2b38ba725532adf07940
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-10-07 14:52:04 +0200

    Add support for tanstack query

--- apps/infra-admin/src/components/hello.tsx
+++ apps/infra-admin/src/components/hello.tsx
@@ -1,21 +1,16 @@
-import { type HelloRequest, HelloWorldService } from "@degov/sdk/pb/hello_pb";
-import { createClient, transport } from "@degov/sdk";
+import { HelloWorldService, type HelloRequest, HelloRequestSchema } from "@degov/sdk/pb/hello_pb";
+import { useQuery } from "@degov/sdk/query";
 import { useState } from "react";
-
-const client = createClient(HelloWorldService, transport);
 
 export const Hello = () => {
     const [name, setName] = useState("");
-    const [message, setMessage] = useState("");
-
-    const handleSubmit = async () => {
-        const response = await client.sayHello({ name });
-        setMessage(response.message);
-    };
+    const { data, refetch } = useQuery(HelloWorldService.method.sayHello, {
+        name: name,
+    });
 
     return <div>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        <button onClick={handleSubmit}>Submit</button>
-        <p>{message}</p>
+        <button onClick={() => refetch()}>Submit</button>
+        <p>{data?.message}</p>
     </div>;
 };
---
+++ packages/sdk/src/query/index.ts
@@ -0,0 +1,2 @@
+export { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+export { TransportProvider, useQuery, useMutation } from "@connectrpc/connect-query";
