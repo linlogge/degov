2b71822c9a6b1476382973376ca47a4760f6f09d
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-11-07 12:02:01 +0100

    Enable ingress on k8s, use sccache and docker cache for local dev

--- bin/frontdoor/src/main.rs
+++ bin/frontdoor/src/main.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use dgv_frontdoor::{Server, ServicesConfig};
 
 #[tokio::main]
@@ -10,21 +8,16 @@ async fn main() -> anyhow::Result<()> {
         let _ = tokio::signal::ctrl_c().await;
     };
 
+    println!("Starting frontdoor server");
+
     let server = Server::builder()
         .with_listen_address("0.0.0.0:8080".parse().unwrap())
         .build()?;
 
-    let (config_sender, serve_watch) = server.serve_watch();
-
-    tokio::spawn(async move {
-        loop {
-            let config_sender = config_sender.clone();
-            tokio::time::sleep(Duration::from_secs(1)).await;
-            let _ = config_sender.send(ServicesConfig::default());
-        }
-    });
-
-    serve_watch.with_graceful_shutdown(cancel_fut).await?;
+    server
+        .serve(ServicesConfig::default())
+        .with_graceful_shutdown(cancel_fut)
+        .await?;
 
     Ok(())
 }
